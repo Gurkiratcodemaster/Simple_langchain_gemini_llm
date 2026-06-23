ded843c5e7ae55ddd353a01fb770740555e54e11
Gurkirat singh <gurkiratjap@gmail.com>
2026-06-23 13:36:28 +0530

    Complete terminal base llm

---
+++ main.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-3.5-flash",
+    temperature = "0.7"
+)
+
+response = llm.invoke("What is langchain?")
+print(response.content[0]["text"])
