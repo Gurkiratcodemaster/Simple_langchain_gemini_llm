b4364ebe1ae7e269b647feb3bb3ad17729ae1675
Gurkirat singh <gurkiratjap@gmail.com>
2026-06-23 14:06:48 +0530

    Add prompt and llm chain

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
 load_dotenv()
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-3.5-flash",
-    temperature = "0.7"
+    temperature = 0.7
 )
-
-response = llm.invoke("What is langchain?")
+prompt = ChatPromptTemplate.from_template(
+    "Explain {topic} in Simple Terms"
+)
+chain = prompt | llm
+response = chain.invoke({"topic":"Agentic AI"})
 print(response.content[0]["text"])
